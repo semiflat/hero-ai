5fa95569a8f10478e51e103c40ba1b01c2ae3999
AdaBan <ada.banaszak@gmail.com>
2023-06-12 14:07:12 +0200

    Adding deployment cofig

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue()],
+  site: 'https://adaban.github.io',
+  base: '/hero-lp',
 });
